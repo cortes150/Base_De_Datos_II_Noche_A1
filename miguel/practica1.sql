0157e252c1ff4a03a0f5d13de587bb1e6de2f01b
MADMASTER-DEV <mad238bigbang@gmail.com>
2026-04-28 21:52:17 -0400

    miguel

--- miguel/practica1.sql
+++ miguel/practica1.sql
@@ -2,3 +2,5 @@
 select * from evento where lugar = "Auditorio";
 -- mostrar entradas vendidas entrada estado vendida
 select * from entrada where estado = "vendida";
+-- mostrar entrada precio mayor a 180 
+select * from pago where monto > 180;
