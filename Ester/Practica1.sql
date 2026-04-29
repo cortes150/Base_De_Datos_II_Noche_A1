329bf6645c40d2320b62c2d94c773b25e8d7cae9
naelb999 <naeninalb999@gmail.com>
2026-04-28 21:56:52 -0400

    ejercicio 4

--- Ester/Practica1.sql
+++ Ester/Practica1.sql
@@ -4,3 +4,5 @@ select * from evento where lugar = "Auditorio";
 select * from entrada where estado = "vendida" ;
 -- mostrar el precio de la entrada mayores a 180
 select * from entrada where precio > 180;
+-- correos que terminen mail.com de usuarios
+select * from usuario where correo like '%mail.com'
