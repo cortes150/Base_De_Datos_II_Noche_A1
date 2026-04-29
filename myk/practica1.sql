6a5c952701af3b22897f059ae9bd509fc8c3e0af
Maykel Lora Salvatierra <maykellora@gmail.com>
2026-04-28 21:56:36 -0400

    ejercicio 1-2-3-4

--- myk/practica1.sql
+++ myk/practica1.sql
@@ -10,3 +10,7 @@ SELECT * from entrada WHERE estado = "vendida";
 -- mostrar el precio mayores a 180;
 
 select * from entrada WHERE precio => 180;
+
+-- correos que terminen en mail.com;
+
+select * from usuario WHERE correo like '%mail.com'
