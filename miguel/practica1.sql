ac7316ad4ca503eff81ec44ba236a6763e638e4d
MADMASTER-DEV <mad238bigbang@gmail.com>
2026-04-28 21:57:02 -0400

    miguel

--- miguel/practica1.sql
+++ miguel/practica1.sql
@@ -4,3 +4,5 @@ select * from evento where lugar = "Auditorio";
 select * from entrada where estado = "vendida";
 -- mostrar entrada precio mayor a 180 
 select * from pago where monto > 180;
+-- correos que tengan mail
+select * from usuario where correo like "%mail.com"
