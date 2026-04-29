62354b42f2798aa9c781560ad0814f29b69e27cf
MADMASTER-DEV <mad238bigbang@gmail.com>
2026-04-28 21:40:52 -0400

    miguel

---
+++ miguel/practica1.sql
@@ -0,0 +1,4 @@
+-- mostrar eventos en el lugar auditorio
+select * from evento where lugar = "Auditorio";
+-- mostrar entradas vendidas entrada estado vendida
+select * from entrada where estado = "vendida";
