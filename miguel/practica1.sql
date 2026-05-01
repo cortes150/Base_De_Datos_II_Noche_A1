2b2d3669f02d2d8ea784995629f067a6ac832cdc
MADMASTER-DEV <mad238bigbang@gmail.com>
2026-04-30 20:33:32 -0400

    miguel

--- miguel/practica1.sql
+++ miguel/practica1.sql
@@ -74,3 +74,26 @@ select * from entrada where estado ="vendida" order by precio desc;
 select * from entrada order by estado asc ,precio desc;
 select * from pago order by metodo asc ,monto desc;
 select * from evento where nombre like "%e%" order by fecha desc ;
+
+-- 30/04/26
+USE unifest_db;
+-- precio entre 80 y 200 ordenadas de mayor a menor
+select * from entrada where precio between 80 and 200 order by precio asc ;
+-- Pagos entre 100 y 250 ordenados de menor a mayor
+select * from pago where monto between 100 and 250 order by monto desc ;
+-- Eventos entre ciertas fechas ordenados por fecha descendente
+select * from evento where fecha between 2025-06-01 and 2025-06-10 order by fecha desc;
+-- Mostrar eventos que NO están en Campus ordenados por nombre
+select * from evento where not lugar ="Capmus" order by nombre desc ;
+-- Pagos que NO son tarjeta ordenados por monto descendente
+select * from pago where not metodo ="tarjeta" order by monto desc ;
+-- Entradas que NO son vendidas ordenadas por precio
+select * from entrada where not estado ="vendida" order by precio asc ;
+-- Ordenar pagos por monto descendente (top pagos primero)
+select * from pago where monto order by monto desc ;
+-- 30/06/2026
+-- pago menor
+select min(monto) as pago_menor from pago;
+-- fecha menor
+select min(fecha) as fecha_menor from evento;
+
